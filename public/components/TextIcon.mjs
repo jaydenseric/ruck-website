e75a5fe068b84e296464dbe14a2ddf2762325459
Jayden Seric <me@jaydenseric.com>
2022-05-09 22:58:36 +1000

    Update Deno, Ruck, and dependencies.

--- public/components/TextIcon.mjs
+++ public/components/TextIcon.mjs
@@ -2,17 +2,13 @@
 
 import { createElement as h } from "react";
 
+/** @typedef {typeof import("device-agnostic-ui/Icon.mjs").default} Icon */
+
 /**
  * React component for an inline text icon.
  * @param {object} props Props.
- * @param {(
- *   props: Omit<
- *     React.ComponentPropsWithoutRef<
- *       typeof import("device-agnostic-ui/Icon.mjs").default
- *     >,
- *     "title"
- *   >
- * ) => import("react").ReactElement} props.icon Icon React component.
+ * @param {(props: Omit<React.ComponentPropsWithoutRef<Icon>, "title">) =>
+ *   import("react").ReactElement} props.icon Icon React component.
  * @param {import("react").ReactNode} [props.children] Children.
  */
 export default function TextIcon({ icon, children }) {
